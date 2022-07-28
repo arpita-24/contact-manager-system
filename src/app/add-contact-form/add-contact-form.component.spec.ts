c496eed894b762236f57d1c734e22cbc3ce1ccf9
Arpita Mohanty <89068505+arpita-24@users.noreply.github.com>
2022-07-28 21:00:09 +0530

    Add files via upload

---
+++ src/app/add-contact-form/add-contact-form.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AddContactFormComponent } from './add-contact-form.component';
+
+describe('AddContactFormComponent', () => {
+  let component: AddContactFormComponent;
+  let fixture: ComponentFixture<AddContactFormComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AddContactFormComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AddContactFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/add-contact-form/add-contact-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-contact-form',
+  templateUrl: './add-contact-form.component.html',
+  styleUrls: ['./add-contact-form.component.css']
+})
+export class AddContactFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
